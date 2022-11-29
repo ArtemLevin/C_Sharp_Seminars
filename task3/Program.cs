705ed44a751e4990d5cd7bd6078e1e318f560144
ArtemLevin <python_study_2022@mail.ru>
2022-11-29 13:51:15 +0300

    Решение задачи про дни недели

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,50 +1,45 @@
 ﻿
-int count = 0;
+Console.Write("Введите номер дня недели " );
 
-while (count <5) 
+int a = int.Parse(Console.ReadLine());
 
+
+
+if (a == 3)
 {
-    int a = new Random().Next(1, 100);
-    Console.WriteLine("Введите номер дня недели " + a);
+    Console.WriteLine("День недели Среда");
 
-
-    if (a == 3)
-    {
-     Console.WriteLine("День недели Среда");
-
-    }
-    if (a == 5)
-    {
-    Console.WriteLine("День недели Пятница"); 
-    }
-
-    if (a == 1)
-    {
-        Console.WriteLine("День недели Понедельник");
-
-    }
-    if (a == 2)
-    {
-    Console.WriteLine("День недели Вторник"); 
-    }
-    if (a == 4)
-    {
-    Console.WriteLine("День недели Четверг"); 
-    }
-    if (a == 6)
-    {
-    Console.WriteLine("День недели Суббота"); 
-    }
-
-    if (a == 7)
-    {
-     Console.WriteLine("День недели Воскресенье"); 
-    }
-
-    if (a > 7) 
-    {
-     Console.WriteLine("Введены некорретные данные"); 
-    }
-
-    count ++;
 }
+if (a == 5)
+{
+Console.WriteLine("День недели Пятница"); 
+}
+
+if (a == 1)
+{
+    Console.WriteLine("День недели Понедельник");
+
+}
+if (a == 2)
+{
+Console.WriteLine("День недели Вторник"); 
+}
+if (a == 4)
+{
+Console.WriteLine("День недели Четверг"); 
+}
+if (a == 6)
+{
+Console.WriteLine("День недели Суббота"); 
+}
+
+if (a == 7)
+{
+ Console.WriteLine("День недели Воскресенье"); 
+}
+
+if (a > 7) 
+{
+    Console.WriteLine("Введены некорретные данные"); 
+}
+
