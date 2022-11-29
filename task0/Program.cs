6c72c715605ed366f81927d37a415f7893cbd9b7
ArtemLevin <python_study_2022@mail.ru>
2022-11-29 11:07:32 +0300

    Square of the number

---
+++ task0/Program.cs
@@ -0,0 +1,14 @@
+﻿int a;
+
+int b;
+
+Console.Write("Enter number a = ");
+
+a = Convert.ToInt32(Console.ReadLine ());
+
+b = a*a;
+
+Console.Write("Square of number a is b = ");
+
+Console.Write(b);
+
